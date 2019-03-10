e3ed1907a2d29cf0dc5cd51420ecd70e302b2ff6
Fabian Kirchner <kirchner@posteo.de>
2019-03-10 12:44:01 +0100

    add license

--- src/Declaration.elm
+++ src/Declaration.elm
@@ -3,6 +3,24 @@ module Declaration exposing
     , parse
     )
 
+{-
+
+   Copyright 2019 Fabian Kirchner
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+-}
+
 import Elm.Type exposing (Type(..))
 import Parser
     exposing
@@ -28,6 +46,26 @@ import Parser
         , variable
         )
 import Set
+
+
+
+{-
+
+   Copyright 2019 Fabian Kirchner
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+-}
 
 
 type Declaration
